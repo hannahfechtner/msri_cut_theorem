082064a5f092f926f5dcbd12d29e7a45b4465741
Hannah Fechtner <hfechtne@andrew.cmu.edu>
2023-06-15 13:12:43 -0400

    fixed

--- MyProject/Definitions.lean
+++ MyProject/Definitions.lean
@@ -1,11 +1,8 @@
 import Mathlib.Data.Real.Basic
-<<<<<<< HEAD
 --secret message hehehe
-=======
 
 --secret message hehehe
 
->>>>>>> d3b333f (Separate files)
 namespace sequent_calculus
 
 --Redefine the type of propositons since Prop is not inductively defined.
@@ -56,15 +53,9 @@ example : Complexity (¬ ((&0 ∧ &1) → &0)) = 3 := by trivial
 inductive Proof : List PropForm → PropForm → Type where
   | id : Proof [A] A
   | exfal : Proof [⊥] A
-<<<<<<< HEAD
-  | com : Proof (Γ ++ A :: Δ ++ B :: θ) C → Proof (Γ ++ B :: Δ ++ A :: θ) C 
-  | wek : Proof Γ A → Proof (B :: Γ) A
-  | contr : Proof (A :: A :: Γ) B → Proof (A :: Γ) B
-=======
   | com (Γ Δ : List PropForm) : Proof (Θ ++ Γ ++ Λ ++ Δ ++ Ξ) C → Proof (Θ ++ Δ ++ Λ ++ Γ ++ Ξ) C 
   | wek (Δ : List PropForm) : Proof Γ A → Proof (Δ ++ Γ) A
   | contr (Δ : List PropForm) : Proof (Δ ++ Δ ++ Γ) B → Proof (Δ ++ Γ) B
->>>>>>> d3b333f (Separate files)
   | rimpl : Proof (A :: Γ) B → Proof Γ (A → B)
   | limpl : Proof Γ A →  Proof (B :: Γ) C  → Proof ((A → B) :: Γ) C
   | rconj : Proof Γ A → Proof Γ B → Proof Γ (A ∧ B)
@@ -73,26 +64,14 @@ inductive Proof : List PropForm → PropForm → Type where
   | rdisjl : Proof Γ A  → Proof Γ (A ∨ B)
   | rdisjr : Proof Γ B  → Proof Γ (A ∨ B)
   | ldisj : Proof (A :: Γ) C  → Proof (B :: Γ) C → Proof ((A ∨ B) :: Γ) C 
-<<<<<<< HEAD
-  | cut : Proof Γ A →  Proof (A :: Γ) B → Proof Γ B 
-
---Define cut-free proof trees.
-=======
   | cut : Proof Γ₀ A →  Proof (A :: Γ₁) B → Proof (Γ₀ ++ Γ₁) B 
->>>>>>> d3b333f (Separate files)
 
 inductive Proof_CF : List PropForm → PropForm → Type where
   | id : Proof_CF [A] A
   | exfal : Proof_CF [⊥] A
-<<<<<<< HEAD
-  | com : Proof_CF (Γ ++ A :: Δ ++ B :: θ) C →  Proof_CF (Γ ++ B :: Δ ++ A :: θ) C 
-  | wek : Proof_CF Γ A → Proof_CF (B :: Γ) A
-  | contr : Proof_CF (A :: A :: Γ) B → Proof_CF (A :: Γ) B
-=======
   | com (Γ Δ : List PropForm) : Proof_CF (Θ ++ Γ ++ Λ ++ Δ ++ Ξ) C → Proof_CF (Θ ++ Δ ++ Λ ++ Γ ++ Ξ) C 
   | wek (Δ : List PropForm) : Proof_CF Γ A → Proof_CF (Δ ++ Γ) A
   | contr (Δ : List PropForm) : Proof_CF (Δ ++ Δ ++ Γ) B → Proof_CF (Δ ++ Γ) B
->>>>>>> d3b333f (Separate files)
   | rimpl : Proof_CF (A :: Γ) B → Proof_CF Γ (A → B)
   | limpl : Proof_CF Γ A →  Proof_CF (B :: Γ) C  → Proof_CF ((A → B) :: Γ) C
   | rconj : Proof_CF Γ A → Proof_CF Γ B → Proof_CF Γ (A ∧ B)
@@ -102,15 +81,14 @@ inductive Proof_CF : List PropForm → PropForm → Type where
   | rdisjr : Proof_CF Γ B  → Proof_CF Γ (A ∨ B)
   | ldisj : Proof_CF (A :: Γ) C  → Proof_CF (B :: Γ) C → Proof_CF ((A ∨ B) :: Γ) C 
 
-<<<<<<< HEAD
 --Define maximum cut depth of a given proof tree.
 
 def Depth_Cut {Γ : List PropForm} {A : PropForm} : Proof Γ A →  ℕ
   | Proof.id => 0 
   | Proof.exfal => 0
-  | Proof.com D => Depth_Cut D
-  | Proof.wek D => Depth_Cut D
-  | Proof.contr D => Depth_Cut D
+  | Proof.com _ _ D => Depth_Cut D
+  | Proof.wek _ D => Depth_Cut D
+  | Proof.contr _ D => Depth_Cut D
   | Proof.rimpl D => Depth_Cut D
   | Proof.limpl D E => max (Depth_Cut D) (Depth_Cut E)
   | Proof.rconj D E => max (Depth_Cut D) (Depth_Cut E)
@@ -126,9 +104,9 @@ def Depth_Cut {Γ : List PropForm} {A : PropForm} : Proof Γ A →  ℕ
 def Size_Cut {Γ : List PropForm} {A : PropForm} : Proof Γ A →  ℕ
   | Proof.id => 0 
   | Proof.exfal => 0
-  | Proof.com D => Size_Cut D 
-  | Proof.wek D => Size_Cut D
-  | Proof.contr D => Size_Cut D
+  | Proof.com _ _ D => Size_Cut D 
+  | Proof.wek _ D => Size_Cut D
+  | Proof.contr _ D => Size_Cut D
   | Proof.rimpl D => Size_Cut D
   | Proof.limpl D E => Size_Cut D + Size_Cut E
   | Proof.rconj D E => Size_Cut D + Size_Cut E
@@ -137,18 +115,16 @@ def Size_Cut {Γ : List PropForm} {A : PropForm} : Proof Γ A →  ℕ
   | Proof.rdisjl D => Size_Cut D 
   | Proof.rdisjr D => Size_Cut D
   | Proof.ldisj D E => Size_Cut D + Size_Cut E + 1 
-  | @Proof.cut _ A _ D E => Size_Cut D + Size_Cut E + Complexity A
+  | @Proof.cut _ A _ _ D E => Size_Cut D + Size_Cut E + Complexity A
 
 --A measure to allow recursion on proof trees.
 
 def Data_Cut {Γ : List PropForm} {A : PropForm} (D : Proof Γ A) : ℕ × ℕ := ⟨Depth_Cut D, Size_Cut D⟩ 
-=======
 --Canonical embedding from Proof_CF to Proof.
 
 def CF_to_C {Γ : List PropForm} {A : PropForm} : Proof_CF → Proof 
   --| Proof_CF.id => Proof.id 
   sorry
->>>>>>> d3b333f (Separate files)
 
 --local notation for valid sequents
 
@@ -156,8 +132,4 @@ infixl: 40
   " ⊢ " => Proof
 
 infixl: 40 
-<<<<<<< HEAD
    " ⊢₁ " => Proof_CF
-=======
-   " ⊢₁ " => Proof_CF
->>>>>>> d3b333f (Separate files)
--- MyProject/Lemma.lean
+++ MyProject/Lemma.lean
@@ -2,10 +2,8 @@ import MyProject.Definitions
 import MyProject.Properties
 import MyProject.Size
 
-theorem CF_Regular {Γ : List PropForm} {A : PropForm} : (Γ ⊢₁ A) → (Γ ⊢ A) := by 
+theorem CF_Regular {Γ : List PropForm} {A : PropForm} : (Proof_CF Γ A) → (Proof Γ A) := by 
+  rename_i one two 
   intro h
-  induction h
+  induction h 
   . 
-
-
-
