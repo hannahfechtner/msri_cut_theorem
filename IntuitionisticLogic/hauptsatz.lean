04f426b28a231531b57aa33a2834bcd23534e6ec
Jin Wei <wj142857@gmail.com>
2023-07-11 00:29:17 +0200

    Auxiliary lemmas

--- IntuitionisticLogic/hauptsatz.lean
+++ IntuitionisticLogic/hauptsatz.lean
@@ -52,7 +52,7 @@ def complexity : PropForm → ℕ
 inductive Proof : List PropForm → PropForm → Type where
   | id (A : PropForm) : Proof [A] A
   | exfal (A : PropForm) : Proof [⊥] A
-  | com (X Y Z : List PropForm) : Proof (X ++ A :: Y ++ B :: Z) C → Proof (X ++ B :: Y ++ A :: Z) C 
+  | com (X Y Z : List PropForm) : Proof (X ++ [A] ++ Y ++ [B] ++ Z) C → Proof (X ++ [B] ++ Y ++ [A] ++ Z) C 
   | wek (B : PropForm) : Proof Γ A → Proof (B :: Γ) A
   | contr : Proof (B :: B :: Γ) A → Proof (B :: Γ) A
   | rimpl : Proof (A :: Γ) B → Proof Γ (A → B)
@@ -63,12 +63,12 @@ inductive Proof : List PropForm → PropForm → Type where
   | rdisjr (A : PropForm) : Proof Γ B  → Proof Γ (A ∨ B)
   | rdisjl (B : PropForm) : Proof Γ A  → Proof Γ (A ∨ B)
   | ldisj : Proof (A :: Γ) C  → Proof (B :: Γ) C → Proof ((A ∨ B) :: Γ) C 
-  | cut : Proof Γ A →  Proof (A :: Γ) B → Proof Γ B 
+  | cut : Proof Γ₀ A →  Proof (A :: Γ₁) B → Proof (Γ₀ ++ Γ₁) B 
 
 inductive Proof_CF : List PropForm → PropForm → Type where
   | id (A : PropForm) : Proof_CF [A] A
   | exfal (A : PropForm) : Proof_CF [⊥] A
-  | com (X Y Z : List PropForm) : Proof_CF (X ++ A :: Y ++ B :: Z) C → Proof_CF (X ++ B :: Y ++ A :: Z) C 
+  | com (X Y Z : List PropForm) : Proof_CF (X ++ [A] ++ Y ++ [B] ++ Z) C → Proof_CF (X ++ [B] ++ Y ++ [A] ++ Z) C 
   | wek (B : PropForm) : Proof_CF Γ A → Proof_CF (B :: Γ) A
   | contr : Proof_CF (B :: B :: Γ) A → Proof_CF (B :: Γ) A
   | rimpl : Proof_CF (A :: Γ) B → Proof_CF Γ (A → B)
@@ -116,7 +116,63 @@ def cut_size {Γ : List PropForm} {A : PropForm} : Proof Γ A → ℕ
   | Proof.rdisjl _ p => cut_size p 
   | Proof.rdisjr _ p => cut_size p
   | Proof.ldisj p q => cut_size p + cut_size q
-  | @Proof.cut _ A _ p q => cut_size p + cut_size q + complexity A
+  | @Proof.cut _ A _ _ p q => cut_size p + cut_size q + complexity A
+
+def Proof.scom (X Y Z Γ Δ : List PropForm) {A : PropForm} (p : (X ++ Γ ++ Y ++ Δ ++ Z) ⊢ A) : (X ++ Δ ++ Y ++ Γ ++ Z) ⊢ A := by
+  sorry
+
+def Proof.swek {Γ : List PropForm} {A : PropForm} (Δ : List PropForm ) (p : Γ ⊢ A) : Δ ++ Γ ⊢ A := by
+  sorry
+
+def Proof.scontr {Γ Δ : List PropForm} {A : PropForm} (p : (Δ ++ Δ ++ Γ) ⊢ A) : Δ ++ Γ ⊢ A := by
+  sorry
+
+def Proof_CF.scom (X Y Z Γ Δ : List PropForm) {A : PropForm} (p : (X ++ Γ ++ Y ++ Δ ++ Z) ⊢₁ A) : (X ++ Δ ++ Y ++ Γ ++ Z) ⊢₁ A := by
+  sorry
+--   cases Γ
+--   . cases Δ 
+--     . assumption 
+--     . rename_i P Δ 
+--       cases Y
+--       . simpa [List.append_nil, List.nil_append] using p
+--       . rename_i Q Y 
+--         change X ++ ([P] ++ Δ) ++ ([Q] ++ Y) ++ [] ++ Z ⊢₁ A
+--         rw [List.append_nil, ← List.append_assoc X, ← List.append_assoc, List.append_assoc]
+--         apply Proof_CF.com 
+--         rw [← List.append_assoc] 
+--         apply struct_com_CF
+--         change X ++ [] ++ ([Q] ++ Y) ++ ([P] ++ Δ) ++ Z ⊢₁ A at p
+--         rw [List.append_nil, ← List.append_assoc X, ← List.append_assoc _ [P]] at p
+--         assumption
+--   . rename_i P Γ 
+--     cases Δ 
+--     . cases Y 
+--       . simpa [List.append_nil, List.nil_append] using p
+--       . rename_i Q Y 
+--         change X ++ [] ++ ([Q] ++ Y) ++ ([P] ++ Γ) ++ Z ⊢₁ A
+--         rw [List.append_nil, ← List.append_assoc X, ← List.append_assoc, List.append_assoc]
+--         apply Proof_CF.com 
+--         rw [← List.append_assoc] 
+--         apply struct_com_CF
+--         change X ++ ([P] ++ Γ) ++ ([Q] ++ Y) ++ [] ++ Z ⊢₁ A at p
+--         rw [List.append_nil, ← List.append_assoc X, ← List.append_assoc _ [Q]] at p
+--         assumption
+--     . rename_i Q Δ 
+--       change X ++ ([Q] ++ Δ) ++ Y ++ ([P] ++ Γ) ++ Z ⊢₁ A 
+--       rw [← List.append_assoc X, List.append_assoc _ Δ, ← List.append_assoc, List.append_assoc]
+--       apply Proof_CF.com
+--       rw [← List.append_assoc _ Δ, List.append_assoc _ Y, ← List.append_assoc]
+--       apply struct_com_CF
+--       change  X ++ ([P] ++ Γ) ++ Y ++ ([Q] ++ Δ) ++ Z ⊢₁ A at p
+--       rw [← List.append_assoc X, ← List.append_assoc _ [Q], List.append_assoc _ Y] at p
+--       assumption
+-- termination_by struct_com_CF X Y Z Γ Δ A p => (List.length Γ + List.length Δ) 
+
+def Proof_CF.swek {Γ : List PropForm} {A : PropForm} (Δ : List PropForm) (p : Γ ⊢₁ A) : Δ ++ Γ ⊢₁ A := by
+  sorry
+
+def Proof_CF.scontr {Γ : List PropForm} {A : PropForm} (p : (Δ ++ Δ ++ Γ) ⊢₁ A) : Δ ++ Γ ⊢₁ A := by
+  sorry
 
 lemma rimpl_inv {Γ : List PropForm} {A B : PropForm} : (Γ ⊢ A → B) →  A :: Γ ⊢ B
   | Proof.id _ => Proof.com [] [] [] (Proof.limpl (Proof.id A) (Proof.com [] [] [] (Proof.wek A (Proof.id B))))
@@ -129,8 +185,12 @@ lemma rimpl_inv {Γ : List PropForm} {A B : PropForm} : (Γ ⊢ A → B) →  A 
   | @Proof.lconjr _ X _ P p => Proof.com [] [] X (Proof.lconjr P (Proof.com [] [] X (rimpl_inv p))) 
   | @Proof.lconjl _ X _ Q p => Proof.com [] [] X (Proof.lconjl Q (Proof.com [] [] X (rimpl_inv p)))  
   | @Proof.ldisj P X _ Q p q => Proof.com [] [] X (Proof.ldisj (Proof.com [] [] X (rimpl_inv p)) (Proof.com [] [] X (rimpl_inv q)))
-  | @Proof.cut X P _ p q => Proof.cut (Proof.wek A p) (Proof.com [] [] X (rimpl_inv q))
- 
+  | @Proof.cut X P Y _ p q => by 
+    change [A] ++ X ++ Y ⊢ B 
+    apply Proof.scom [] [] Y X [A]
+    have ic := Proof.cut p (Proof.com [] [] Y (rimpl_inv q))
+    simpa using ic 
+
 lemma rconj_inv {Γ : List PropForm} {A B : PropForm} : (Γ ⊢ A ∧ B) → Γ ⊢ A × Γ ⊢ B
   | Proof.id _ => (Proof.lconjl B (Proof.id A), Proof.lconjr A (Proof.id B))
   | Proof.exfal _ => (@Proof.exfal A, @Proof.exfal B)
@@ -170,10 +230,15 @@ theorem hauptsatz {Γ : List PropForm} {A : PropForm} : (Γ ⊢ A) → Γ ⊢₁
   | Proof.rdisjr A p => Proof_CF.rdisjr A (hauptsatz p)
   | Proof.rdisjl B p => Proof_CF.rdisjl B (hauptsatz p)
   | Proof.ldisj p q => Proof_CF.ldisj (hauptsatz p) (hauptsatz q)
-  | @Proof.cut _ B _ p q => match B with 
+  | @Proof.cut Γ₀ B Γ₁ _ p q => match B with 
     | var n => match p with 
-      | Proof.id _ => by sorry
+      | Proof.id _ => hauptsatz q
       | Proof.exfal _ => by sorry
+        -- change [] ++ [fls] ++ [] ++ Γ₁ ⊢₁ A
+        -- rw [← List.append_nil ([] ++ [fls] ++ [] ++ Γ₁)]
+        -- apply Proof_CF.scom [] [] [] Γ₁ [fls]
+        -- rw [List.nil_append, List.append_nil, List.append_nil]
+        -- apply (Proof_CF.swek Γ₁ (Proof_CF.exfal A))
       | Proof.com _ _ _ _ => by sorry
       | Proof.wek _ _ => by sorry
       | Proof.contr _ => by sorry
@@ -183,38 +248,45 @@ theorem hauptsatz {Γ : List PropForm} {A : PropForm} : (Γ ⊢ A) → Γ ⊢₁
       | Proof.ldisj _ _ => by sorry
       | Proof.cut _ _ => by sorry
     | fls => match p with 
-      | Proof.id _ => by sorry
+      | Proof.id _ => hauptsatz q
       | Proof.exfal _ => by sorry
       | Proof.com _ _ _ _ => by sorry
-      | Proof.wek _ _ => by sorry
-      | Proof.contr _ => by sorry
+      | @Proof.wek X _ C r => by sorry
+      | @Proof.contr C X _ r => by sorry 
       | Proof.limpl _ _ => by sorry
       | Proof.lconjr _ _ => by sorry
       | Proof.lconjl _ _ => by sorry
       | Proof.ldisj _ _ => by sorry
       | Proof.cut _ _ => by sorry
     | impl P Q => by 
-      generalize g : (P → Q) :: Γ = Δ 
+      generalize g : (P → Q) :: Γ₁ = Δ 
       rw [g] at q
       cases q 
-      . sorry
-      . sorry
-      . sorry
-      . sorry 
-      . sorry
-      . sorry      
-      . sorry 
-      . sorry
+      . injection g with gh 
+        rw [gh] at p 
+        sorry 
+      . injection g
+        contradiction 
       . sorry
       . sorry 
       . sorry
       . sorry      
+      . sorry 
       . sorry
+      . injection g
+        contradiction 
+      . injection g
+        contradiction 
       . sorry
-    | conj P Q => hauptsatz (Proof.cut (rconj_inv p).2 (Proof.cut (Proof.wek Q (rconj_inv p).1) (lconj_inv q)))
+      . sorry      
+      . injection g
+        contradiction 
+      . sorry
+    | conj P Q => by sorry
+      --have := hauptsatz (Proof.cut (rconj_inv p).2 (Proof.cut ((rconj_inv p).1) (lconj_inv q)))
     | disj P Q => match p with 
-      | Proof.id _ => by sorry
-      | Proof.exfal _ => by sorry      
+      | Proof.id _ => hauptsatz q
+      | Proof.exfal _ => by sorry 
       | Proof.com _ _ _ _ => by sorry
       | Proof.wek _ _ => by sorry
       | Proof.contr _ => by sorry
@@ -225,4 +297,4 @@ theorem hauptsatz {Γ : List PropForm} {A : PropForm} : (Γ ⊢ A) → Γ ⊢₁
       | Proof.rdisjl _ _ => by sorry
       | Proof.ldisj _ _ => by sorry
       | Proof.cut _ _ => by sorry
-termination_by hauptsatz p => (cut_size p, size p)  
+termination_by hauptsatz p => (cut_size p, size p)
